2907836d18365fdcabdf192ab5a8f31182f4bfa5
okezino <okezi002@gmail.com>
2022-06-18 10:53:42 +0100

    network-database

---
+++ app/src/main/java/com/codinginflow/mvvmnewsapp/data/NewsArticleDao.kt
@@ -0,0 +1,22 @@
+package com.codinginflow.mvvmnewsapp.data
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+interface NewsArticleDao {
+
+    @Query("SELECT * FROM breaking_news INNER JOIN news_articles ON articleUrl = url")
+    fun getAllBreakingNews() : Flow<List<NewsArticle>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArticles(article: List<NewsArticle>)
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBreakingNews(breakingNews: List<BreakingNews>)
+
+    @Query("DELETE FROM breaking_news")
+    suspend fun deleteAllBreakingNews()
+}
